ede6c77cfb6798ef3164613c750923053e725e7d
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2019-06-30 10:32:58 +0900

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -11,6 +11,7 @@ class Graph:
     def print_Graph(self):
         print(self.g)
 
+    #가중치 없는 간선 삽입 그래프 만들때 사용
     def insert_edge(self,start,end):
         if start == end:
             return
@@ -21,5 +22,18 @@ class Graph:
         else:
             self.g[start][end] = 1
             self.g[end][start] = 1
+
+    #가중치 있는 간선 삽입 그래프 만들때 사용
+    def insert_edge_weight(self,start,end,weight):
+        if start == end:
+            return
+        elif start >= self.size or end >= self.size:
+            return
+        elif self.g[start][end] != 0 and g[end][start] != 0:
+            return
+        else:
+            self.g[start][end] = weight
+            self.g[end][start] = weight
+
     def Graph_return(self):
         return self.g
