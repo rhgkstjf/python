b75c4b1f096d9225d2d7006a9bfa6391fe40b405
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2021-05-01 21:34:37 +0900

    Update code.py
    굿

--- programmers/level3/네트워크/code.py
+++ programmers/level3/네트워크/code.py
@@ -1,33 +1,21 @@
 from collections import deque
-
+    
 def solution(n, computers):
     answer = 0
     
     visited = [0]*n
-    stack = deque()
-    stack.append(0)
+    queue = deque()
     
-    while stack:
-        net_num= stack.popleft()
-        network = computers[net_num]
-        for conn_index, conn in enumerate(network):
-            #print(conn_index)
-            if conn == 1:
-                if conn_index != net_num and visited[conn_index] == 0:
-                    visited[conn_index] = 1
-                    #print('{0}번 네트워크 방문'.format(conn_index))
-                    stack.append(conn_index)
-                    
-    lastnum = visited[0]
-    for i in visited[1:]:
-        if i == 0:
-            answer += 1
-            if lastnum == 1:
-                answer += 1
-        lastnum = i
-        
-    if lastnum == 1:
+    while 0 in visited:
+        not_clear = visited.index(0)
+        queue.append(not_clear)
+        visited[not_clear] = 1
+        while queue:
+            network_num = queue.popleft()
+            for i in range(n):
+                if visited[i] == 0 and computers[network_num][i] == 1:
+                    queue.append(i)
+                    visited[i] = 1
         answer += 1
-
-    
+        
     return answer
