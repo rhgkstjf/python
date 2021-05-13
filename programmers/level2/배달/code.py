5be5a9befd578e8556c47687df8b13ab60168e7f
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2021-05-13 20:06:42 +0900

    Create code.py
    테스트 케이스30,31,32 번이 실패했다. 이유를 찾고있다.

---
+++ programmers/level2/배달/code.py
@@ -0,0 +1,26 @@
+import heapq
+
+def solution(N, road, K):
+    answer = 0
+    
+    dist = [10001]*(N+1)
+    PriorityQ = []
+    heapq.heappush(PriorityQ, (1,0))
+    dist[1] = 0
+    
+    while PriorityQ:
+        cnode, c_weight = heapq.heappop(PriorityQ)
+        for start, end, weight in road:
+            cost_evl = c_weight + weight
+            if start == cnode and cost_evl < dist[end]:
+                dist[end] = cost_evl
+                heapq.heappush(PriorityQ, (end, cost_evl))
+            if end == cnode and cost_evl < dist[start]:
+                dist[start] = cost_evl
+                heapq.heappush(PriorityQ, (start, cost_evl))
+    
+    answer = len([i for i in dist if i <= K])
+            
+        
+
+    return answer
